0d6b4f32b8fe35248a36260e81a2edadd0f72a8b
hiscaler <hiscaler@gmail.com>
2018-03-23 14:21:35 +0800

    ...

--- url/cnn.py
+++ url/cnn.py
@@ -1,7 +1,8 @@
 # -*- encode: utf-8 -*-
-
-
+"""CNN 网站数据地址采集
+"""
 import re
+import time
 
 import requests
 from bs4 import BeautifulSoup
@@ -9,38 +10,52 @@ from selenium import webdriver
 
 url = 'https://edition.cnn.com/'
 language = 'en-US'
-options = webdriver.ChromeOptions()
-options.add_argument('user-data-dir=D:/tmp')
-# options.add_argument('-headless')
-browser = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver', options=options)
-browser.get(url)
-html = browser.page_source
-if html:
-    soup = BeautifulSoup(html)
-    for script in soup(["script", "style"]):
-        script.decompose()
 
-    links = soup.find_all('a', href=True)
-    pattern = re.compile(r'.*href="(\S*)".*')
-    for link in links:
-        link = link.prettify()
-        href = pattern.findall(link)
-        if href:
-            href = href[0]
-            if href and href.endswith('index.html'):
-                if not href.startswith('http'):
-                    href = url + href
+urls = [
+    'https://edition.cnn.com/',
+    'https://edition.cnn.com/travel',
+]
 
-                print("Url: %s" % href)
+begin_time = time.time()
+try:
+    options = webdriver.ChromeOptions()
+    options.add_argument('user-data-dir=D:/tmp')
+    options.add_argument('-headless')
+    options.add_argument('-disable-gpu')
+    browser = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver', options=options)
+    open_browser_time = time.time()
+    for url in urls:
+        browser.get(url)
+        html = browser.page_source
+        if html:
+            soup = BeautifulSoup(html)
+            for script in soup(["script", "style"]):
+                script.decompose()
 
-                response = requests.post('http://localhost:8002/index.php/api/post/url/create', data={"language": language, "url": href})
-                if response.ok:
-                    body = response.json()
-                    if body['success']:
-                        print("Success: %s" % href)
-                    else:
-                        print("Failed: %s" % href)
-                else:
-                    print(response.text)
+            links = soup.find_all('a', href=True)
+            pattern = re.compile(r'.*href="(\S*)".*')
+            for link in links:
+                link = link.prettify()
+                href = pattern.findall(link)
+                if href:
+                    href = href[0]
+                    if href and href.endswith('index.html'):
+                        if not href.startswith('http'):
+                            href = url + href
 
-browser.close()
+                        print("Url: %s" % href)
+
+                        response = requests.post('http://localhost:8002/index.php/api/post/url/create', data={"language": language, "url": href})
+                        if response.ok:
+                            body = response.json()
+                            if body['success']:
+                                print("Success: %s" % href)
+                            else:
+                                print("Failed: %s" % href)
+                        else:
+                            print(response.text)
+finally:
+    browser.close()
+
+print("Open browser cost %s seconds" % (open_browser_time - begin_time))
+print("Cost %s Seconds" % (time.time() - begin_time))
